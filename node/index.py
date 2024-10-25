575fdbf3f32b3c68b98b8362974f4231f952933f
Cao Ngoc Lam <lam.cao1206@hcmut.edu.vn>
2024-10-25 17:02:18 +0700

    init hello world message

--- node/index.py
+++ node/index.py
@@ -1,5 +1,26 @@
+import socket
+import sys
+
+
+class Client:
+    def __init__(self, host="localhost", port=8000) -> None:
+        self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.sock.connect(("localhost", 8000))
+
+    def start(self):
+        msg = self.sock.recv(1024)
+        print(msg)
+
+        while msg:
+            print("Received ", msg.decode())
+            msg = self.sock.recv(1024)
+
+
 def main():
-    pass
+    host = sys.argv[1] if len(sys.argv) > 1 else "localhost"
+    port = int(sys.argv[2]) if len(sys.argv) > 2 else 8000
+    client = Client(host, port)
+    client.start()
 
 
 if __name__ == "__main__":
--- tracker/index.py
+++ tracker/index.py
@@ -1,5 +1,31 @@
+import socket
+import yaml
+
+
+class Tracker:
+    def __init__(self, host="localhost", port=8000, max_clients=10) -> None:
+        self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.sock.bind((host, port))
+        self.sock.listen(max_clients)
+        print(f"Tracker listening on {host}:{port}")
+
+    def start(self):
+        while True:
+            client, addr = self.sock.accept()
+            print(f"Connect from {str(addr)}")
+            client.send(b"Hello from Tracker")
+            client.close()
+
+
 def main():
-    pass
+    with open("config.yaml", "r") as file:
+        config = yaml.safe_load(file)
+
+    host = config["server"]["host"]
+    port = config["server"]["port"]
+    max_clients = config["server"]["max_clients"]
+    tracker = Tracker(host, port, max_clients)
+    tracker.start()
 
 
 if __name__ == "__main__":
